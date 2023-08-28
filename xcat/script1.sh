adcf9fa576366a2b42520bd0ecc105c969247e43
nityavats <130952232+nityavats@users.noreply.github.com>
2023-08-28 10:07:25 -0400

    Add files via upload

---
+++ xcat/script1.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+#user=`whoami`
+#$user ALL=(ALL) NOPASSWD: /home/part0.sh | sudo tee -a /usr/sbin/visudo
+
+
+####################################################################################################################################################################################################
+
+
+#PART 1
+ifup ens33
+
+whiptail --msgbox "Welcome to Setup wizard of xCAT. Press Enter to continue." 10 50
+yum install zenity -y
+
+
+#Disable Firewall
+systemctl stop firewalld.service; systemctl stop firewalld
+systemctl disable firewalld.service; systemctl disable firewalld
+
+#Disable SELINUX
+setenforce 0
+sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/sysconfig/selinux
+
+#Disable DHCP on the interface
+private_interface=$(zenity --entry --title="Private network's interface required" --text="Please mention the interface of private network : ")
+mkdir -p /root/inputs
+touch /root/inputs/private_interface
+echo "$private_interface" > /root/inputs/private_interface
+
+
+#read -p "Please mention the interface of private network : " private_interface
+#sed -i '/BOOTPROTO/d' /etc/sysconfig/network-scripts/ifcfg-$private_interface
+#echo "BOOTPROTO=none" >> /etc/sysconfig/network-scripts/ifcfg-$private_interface
+
+#Taking inputs Hostname, IP, Domain name
+
+hostname_master=$(zenity --entry --title="Hostname required" --text="Provide a hostname to your master.")
+touch /root/inputs/hostname_master
+echo "$hostname_master" > /root/inputs/hostname_master
+#read -p "Provide a hostname to your master : " hostname_master
+
+
+privateip_network=$(ip a show dev $private_interface | grep -oP 'inet \K[\d.]+')
+echo "nameserver $privateip_network" >> /etc/resolv.conf    #this command not running in bash
+touch /root/inputs/private_ip
+echo "$privateip_network" > /root/inputs/private_ip
+
+zenity \
+--info \
+--text="current ip is $privateip_network.\n DO NOT CHANGE THE IP" \
+--title="xCAT setup" \
+--ok-label="OK"
+                                   
+#whiptail --msgbox "Your current Private ip is $privateip_network. Press Enter & retype the same ip LOL." 10 50
+#read -p "Retype Private ip of your Master Node : " master_privateip
+       
+domain_name=$(zenity --entry --title="Domain name required" --text="Please provide a domain name.")
+touch /root/inputs/domain_name
+echo "$domain_name" > /root/inputs/domain_name
+                          
+#read -p "Please provide a domain name --Example 'avicii.in' -- : " domain_name               
+hostnamectl set-hostname $hostname_master.$domain_name		                                                   
+
+#Changing network
+#privateip_network=$(ip a show dev $private_interface | grep -oP 'inet \K[\d.]+')
+#read -p "Provide Private ip to your Master Node : " master_privateip
+#ip addr add $master_privateip/255.255.255.0 dev ens34
+#sed -i 's/IPADDR=$privateip_network/IPADDR=$master_privateip/g' /etc/sysconfig/network-scripts/ifcfg-$private_interface
+#echo "BOOTPROTO=none" >> /etc/sysconfig/network-scripts/ifcfg-$private_interface
+#ifdown $private_interface
+#ifup $private_interface
+
+#Assigning values
+echo "$privateip_network $hostname_master $hostname_master.$domain_name" >> /etc/hosts
+#echo "nameserver $privateip_network" >> /etc/resolv.conf    #this command not running in bash
+#echo "nameserver $privateip_network" | sudo tee -a /etc/resolv.conf      #This runs but you need to enter password
+
+whiptail --msgbox "Machine needs a reboot, please press Enter to continue." 10 50
+
+exec bash | bash script2.sh 
---
+++ xcat/script2.sh
@@ -0,0 +1,26 @@
+#PART 2
+
+
+#!/bin/bash
+
+
+#Updating yum and Utilities
+yum install zenity -y
+yum install xterm -y
+yum install ntp -y
+yum update -y
+yum install yum-utils -y
+wget -P /etc/yum.repos.d https://xcat.org/files/xcat/repos/yum/latest/xcat-core/xcat-core.repo --no-check-certificate
+wget -P /etc/yum.repos.d https://xcat.org/files/xcat/repos/yum/xcat-dep/rh7/x86_64/xcat-dep.repo --no-check-certificate
+yum install epel-release -y
+yum clean all
+yum makecache
+yum install xCAT -y
+#chmod 777 /etc/profile.d/xcat.sh
+
+
+#SOURCE
+source /etc/profile.d/xcat.sh
+cat /etc/profile.d/xcat.sh
+
+bash script3.sh
